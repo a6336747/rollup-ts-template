21f651d62fa4ab25daebfcdb968840f71d18c219
sakuya <a282363084@qq.com>
2023-08-24 18:24:03 +0800

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import rollupTypescript from 'rollup-plugin-typescript2'
 import babel from '@rollup/plugin-babel'
 import { DEFAULT_EXTENSIONS } from '@babel/core'
 import { terser } from 'rollup-plugin-terser'
+import dts from "rollup-plugin-dts";
 
 // 读取 package.json 配置
 import pkg from './package.json'
@@ -13,50 +14,10 @@ const env = process.env.NODE_ENV
 // umd 模式的编译结果文件输出的全局变量名称
 const name = 'RollupTsTemplate'
 const config = { 
-  // 入口文件，src/index.ts
-  input: path.resolve(__dirname, 'src/index.ts'),
-  // 输出文件
-  output: [
-    // commonjs
-    {
-      // package.json 配置的 main 属性
-      file: pkg.main,
-      format: 'cjs',
-    },
-    // es module
-    {
-      // package.json 配置的 module 属性
-      file: pkg.module,
-      format: 'es',
-    },
-    // umd
-    {
-      // umd 导出文件的全局变量
-      name,
-      // package.json 配置的 umd 属性
-      file: pkg.umd,
-      format: 'umd'
-    }
-  ],
+  input: "./dist/types/index.d.ts",
+  output: [{ file: "dist/types/redis.d.ts", format: "es" }],
   plugins: [
-    // 解析第三方依赖
-    resolve(),
-    // 识别 commonjs 模式第三方依赖
-    commonjs(),
-    // rollup 编译 typescript
-    rollupTypescript(),
-    // babel 配置
-    babel({
-      // 编译库使用 runtime
-      babelHelpers: 'runtime',
-      // 只转换源代码，不转换外部依赖
-      exclude: 'node_modules/**',
-      // babel 默认不支持 ts 需要手动添加
-      extensions: [
-        ...DEFAULT_EXTENSIONS,
-        '.ts',
-      ],
-    }),
+    dts(),
   ]
 }
 
